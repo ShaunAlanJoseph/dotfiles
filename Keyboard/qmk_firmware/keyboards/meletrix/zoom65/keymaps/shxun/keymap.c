aecd5af199932f570e7477f7556ae65c84d43dfe
Shaun Alan Joseph <shaun.alan.joseph.s@gmail.com>
2025-07-20 04:06:35 +0530

    update: keyboard: corrected rotary encoder keymaps

--- Keyboard/qmk_firmware/keyboards/meletrix/zoom65/keymaps/shxun/keymap.c
+++ Keyboard/qmk_firmware/keyboards/meletrix/zoom65/keymaps/shxun/keymap.c
@@ -24,32 +24,47 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_all(
-        KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  KC_MUTE,
-        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PSCR,
-        LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_MPLY,
-        KC_LSFT,        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_CAPS,
-        KC_LCTL,        KC_LGUI, KC_LALT,                   MO(1),   MO(2),   KC_SPC,           KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
+    // +--------1--------2--------3--------4--------5--------6--------7--------8--------9--------0--------+--------+--------+--------+--------+
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  KC_MUTE,
+    // +--------Q--------W--------E--------R--------T--------Y--------U--------I--------O--------P--------+--------+--------+--------+--------+
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS, KC_PSCR,
+    // +--------A--------S--------D--------F--------G--------H--------J--------K--------L--------+--------+--------+--------+--------+--------+
+ LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,  KC_MPLY,
+    // +--------+--------Z--------X--------C--------V--------B--------N--------M--------+--------+--------+--------+--------+--------+--------+
+        KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_CAPS,
+    // +--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
+        KC_LCTL, KC_LGUI, KC_LALT,                   MO(1),   MO(2),   KC_SPC,           KC_RALT, KC_RCTL,                   KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_FIRST] = LAYOUT_all(
+    // +--------1--------2--------3--------4--------5--------6--------7--------8--------9--------0--------+--------+--------+--------+--------+
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,                   _______, _______, _______,          _______, _______,          _______, _______, _______
+    // +--------Q--------W--------E--------R--------T--------Y--------U--------I--------O--------P--------+--------+--------+--------+--------+
+        _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, _______,          _______, _______,
+    // +--------A--------S--------D--------F--------G--------H--------J--------K--------L--------+--------+--------+--------+--------+--------+
+        _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,                   _______, _______,
+    // +--------+--------Z--------X--------C--------V--------B--------N--------M--------+--------+--------+--------+--------+--------+--------+
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
+    // +--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
+        _______, _______, _______,                   _______, _______, _______,          _______, _______,                   _______, _______, _______
     ),
     [_SECOND] = LAYOUT_all(
+    // +--------1--------2--------3--------4--------5--------6--------7--------8--------9--------0--------+--------+--------+--------+--------+
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MS_BTN1, MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, MS_BTN2, XXXXXXX,          XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, _______, XXXXXXX,          XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX
+    // +--------Q--------W--------E--------R--------T--------Y--------U--------I--------O--------P--------+--------+--------+--------+--------+
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX,
+    // +--------A--------S--------D--------F--------G--------H--------J--------K--------L--------+--------+--------+--------+--------+--------+
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MS_BTN1, MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, MS_BTN2, XXXXXXX,                   XXXXXXX, XXXXXXX,
+    // +--------+--------Z--------X--------C--------V--------B--------N--------M--------+--------+--------+--------+--------+--------+--------+
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX,
+    // +--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
+        XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, _______, XXXXXXX,          XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX
     )
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_BASE] = {
-        ENCODER_CCW_CW(MS_WHLU, MS_WHLD)
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)
     },
     [_FIRST] = {
         ENCODER_CCW_CW(KC_BRID, KC_BRIU)
